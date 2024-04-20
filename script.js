ebca4bbd79fe0c5f15d6a8b01857877395effb71
Yago Raímon <yagoraimon@gmail.com>
2024-04-19 21:41:29 -0300

    new api for location
    uma nova API para iniciar o app com a cidade atual do usuário.
    
    Unidades de temperatura dinâmicas.

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Inicializações e definições de variáveis
-  let currCity = "Recife"
-  let units = "metric"
-  const searchForm = document.querySelector(".weather__searchform")
+  let currCity,
+    units = "metric"
+  const form = document.querySelector(".weather__search")
+  const searchInput = form.querySelector("input[type='text']") // Seleciona o input de texto dentro do formulário
+  const weatherCity = document.querySelector(".weather__city")
+  const datetime = document.querySelector(".weather__datetime")
+  const weatherForecast = document.querySelector(".weather__forecast")
+  const weatherTemperature = document.querySelector(".weather__temperature")
+  const weatherIcon = document.querySelector(".weather__icon")
+  const weatherMinMax = document.querySelector(".weather__minmax")
+  const weatherRealFeel = document.querySelector(".weather__realfeel")
+  const weatherHumidity = document.querySelector(".weather__humidity")
+  const weatherWind = document.querySelector(".weather__wind")
+  const weatherPressure = document.querySelector(".weather__pressure")
 
-  // Definir e adicionar manipuladores de evento
-  document.querySelector(".weather__search").addEventListener("submit", (e) => {
+  // Event listener para o formulário de busca
+  form.addEventListener("submit", (e) => {
     e.preventDefault()
-    currCity = searchForm.value
-    getWeather()
-    searchForm.value = ""
+    if (searchInput.value.trim()) {
+      currCity = searchInput.value.trim() // Atualiza a cidade atual com a nova pesquisa
+      getWeather()
+      searchInput.value = "" // Limpa o campo de entrada após a submissão
+    } else {
+      alert("Por favor, insira um nome de cidade válido.")
+    }
   })
 
+  // Adiciona event listeners para a mudança de unidades
   document
     .querySelector(".weather__unit_celsius")
     .addEventListener("click", () => {
@@ -30,28 +45,77 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     })
 
-  // Chamada inicial da função para carregar dados
-  getWeather()
+  // Chamada inicial para carregar os dados de clima baseados na localização por IP
+  fetch("https://ipapi.co/json/")
+    .then((response) => response.json())
+    .then((jsonData) => {
+      currCity = jsonData.city
+      getWeather()
+    })
 
-  // Selectors
-  let city = document.querySelector(".weather__city")
-  let datetime = document.querySelector(".weather__datetime")
-  let weather__forecast = document.querySelector(".weather__forecast")
-  let weather__temperature = document.querySelector(".weather__temperature")
-  let weather__icon = document.querySelector(".weather__icon")
-  let weather__minmax = document.querySelector(".weather__minmax")
-  let weather__realfeel = document.querySelector(".weather__realfeel")
-  let weather__humidity = document.querySelector(".weather__humidity")
-  let weather__wind = document.querySelector(".weather__wind")
-  let weather__pressure = document.querySelector(".weather__pressure")
+  function getWeather() {
+    if (!currCity) {
+      alert("Nome da cidade não fornecido.")
+      return
+    }
+    const API_KEY = "64f60853740a1ee3ba20d0fb595c97d5"
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}&lang=pt_br`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`Erro HTTP: Status ${res.status}`)
+        }
+        return res.json()
+      })
+      .then((data) => {
+        if (!data || !data.weather) {
+          alert("Dados de clima não disponíveis para esta cidade.")
+          return
+        }
+        updateWeatherDisplay(data)
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar os dados de clima: ", error)
+        alert(`Erro ao buscar dados do clima. ${error.message}`)
+      })
+  }
 
+  function updateWeatherDisplay(data) {
+    weatherCity.innerHTML = `${data.name}, ${convertCountryCode(
+      data.sys.country
+    )}`
+    datetime.innerHTML = convertTimeStamp(data.dt, data.timezone)
+    weatherForecast.innerHTML = `<p>${data.weather[0].description}</p>`
+    weatherTemperature.innerHTML = `${data.main.temp.toFixed()}${
+      units === "imperial" ? "&#176F" : "&#176C"
+    }`
+    weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" />`
+    weatherMinMax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}${
+      units === "imperial" ? "&#176F" : "&#176C"
+    }</p><p>Max: ${data.main.temp_max.toFixed()}${
+      units === "imperial" ? "&#176F" : "&#176C"
+    }</p>`
+    weatherRealFeel.innerHTML = `${data.main.feels_like.toFixed()}${
+      units === "imperial" ? "&#176F" : "&#176C"
+    }`
+    weatherHumidity.innerHTML = `${data.main.humidity}%`
+    weatherWind.innerHTML = `${data.wind.speed} ${
+      units === "imperial" ? "mph" : "m/s"
+    }`
+    weatherPressure.innerHTML = `${data.main.pressure} hPa`
+  }
+
+  // Função para converter o código de país em nome de país
+  function convertCountryCode(countryCode) {
+    const regionNames = new Intl.DisplayNames(["pt-BR"], { type: "region" })
+    return regionNames.of(countryCode)
+  }
+
+  // Função para converter o timestamp UNIX em data local
   function convertTimeStamp(timestamp, timezoneOffset) {
-    // Criar uma data UTC com o timestamp fornecido
     const utcDate = new Date(timestamp * 1000)
-    // Aplicar o offset do fuso horário (convertido para milissegundos)
     const localDate = new Date(utcDate.getTime() + timezoneOffset * 1000)
-
-    // Configurar opções de formatação com o fuso horário local do usuário
     const options = {
       weekday: "long",
       day: "numeric",
@@ -59,41 +123,8 @@ document.addEventListener("DOMContentLoaded", function () {
       year: "numeric",
       hour: "2-digit",
       minute: "2-digit",
-      timeZone: "UTC", // Usar UTC aqui para aplicar manualmente o offset
+      timeZone: "UTC",
     }
-
-    // Formatar a data para a localidade 'pt-BR'
     return localDate.toLocaleString("pt-BR", options)
   }
-
-  // convert country code to name
-  function convertCountryCode(country) {
-    let regionNames = new Intl.DisplayNames(["pt-BR"], { type: "region" })
-    return regionNames.of(country)
-  }
-
-  function getWeather() {
-    const API_KEY = "64f60853740a1ee3ba20d0fb595c97d5"
-
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}&lang=pt_br`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data)
-        city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`
-        datetime.innerHTML = convertTimeStamp(data.dt, data.timezone)
-        weather__forecast.innerHTML = `<p>${data.weather[0].description}`
-        weather__temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
-        weather__icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
-        weather__minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p><p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
-        weather__realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
-        weather__humidity.innerHTML = `${data.main.humidity}%`
-        weather__wind.innerHTML = `${data.wind.speed} ${
-          units === "imperial" ? "mph" : "m/s"
-        }`
-        weather__pressure.innerHTML = `${data.main.pressure} hPa`
-      })
-  }
-
 })
